622b2a84c346a498320b4d3c9798d00c28688b85
Phondba <onkar.phondba@capgemini.com>
2016-09-08 12:36:36 +0530

    Signed-off-by: Phondba <onkar.phondba@capgemini.com>

--- src/test/java/Sample/PetClinicTest.java
+++ src/test/java/Sample/PetClinicTest.java
@@ -20,7 +20,7 @@ public class PetClinicTest {
    public void beforeTest() {
 	//System.setProperty("webdriver.chrome.driver", "//home//ec2-user//chromedriver");  
 	//driver = new ChromeDriver(); 
- File pathToBinary = new File("//usr//local//firefox//firefox-bin");
+ File pathToBinary = new File("//usr//local//firefox//firefox-bin.exe");
  FirefoxBinary ffBinary = new FirefoxBinary(pathToBinary);
  FirefoxProfile firefoxProfile = new FirefoxProfile();
  FirefoxDriver driver = new FirefoxDriver(ffBinary,firefoxProfile);
