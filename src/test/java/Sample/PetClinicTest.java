3c5eaf652e59d12798d8be74952e8d6768692fb0
Phondba <onkar.phondba@capgemini.com>
2016-09-12 16:57:47 +0530

    Signed-off-by: Phondba <onkar.phondba@capgemini.com>

--- src/test/java/Sample/PetClinicTest.java
+++ src/test/java/Sample/PetClinicTest.java
@@ -3,6 +3,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -72,15 +73,15 @@ public class PetClinicTest {
 		driver.findElement(By.id("address")).sendKeys("99th Street");
 		driver.findElement(By.id("city")).sendKeys("Mumbai");
 		driver.findElement(By.id("telephone")).sendKeys("4567891230");
-//		driver.findElement(By.xpath("//div[@class='col-sm-offset-2 col-sm-10']/button")).click();
-//		driver.findElement(By.xpath("//div[@class='container xd-container']/a[2]")).click();
-//		driver.findElement(By.id("name")).sendKeys("Ghost");
-//		driver.findElement(By.id("birthDate")).sendKeys("2015/05/05");
-//		driver.findElement(By.xpath("//div[@class='container-fluid']")).click();
-//		Select select=new Select(driver.findElement(By.id("type")));
-//		select.selectByVisibleText("dog");
-//		
-//		driver.findElement(By.xpath("//form//div/button[@class='btn btn-default']")).click();
+		driver.findElement(By.xpath("//div[@class='col-sm-offset-2 col-sm-10']/button")).click();
+		driver.findElement(By.xpath("//div[@class='container xd-container']/a[2]")).click();
+		driver.findElement(By.id("name")).sendKeys("Ghost");
+		driver.findElement(By.id("birthDate")).sendKeys("2015/05/05");
+		driver.findElement(By.xpath("//div[@class='container-fluid']")).click();
+		Select select=new Select(driver.findElement(By.id("type")));
+		select.selectByVisibleText("dog");
+		
+		driver.findElement(By.xpath("//form//div/button[@class='btn btn-default']")).click();
        
 		
     }
